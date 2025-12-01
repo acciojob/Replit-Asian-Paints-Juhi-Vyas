84a2b15b894f02eb1d1ffe4cd3aa8ebbdb0f1f07
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2025-12-01 13:13:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let gridItem = document.querySelectorAll("#grid-item")
+let changeBtn = document.getElementById("change_button")
+let input = document.getElementById("block_id")
+let color = document.getElementById("colour_id")
+
+
+changeBtn.addEventListener("click", ()=>{
+	let inputValue = input.value
+	let colorValue = color.value
+	gridItem[inputValue-1].style.backgroundColor = colorValue
+})
+
